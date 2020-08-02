62c381b2b84f5564bc5d42ae3d14287f11a833cd
re-arep <36735940+re-arep@users.noreply.github.com>
2020-08-03 05:23:54 +0900

    update file 3

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import load_data
+import models
+
+net = models()
 
 trainloader, testloader, classes = load_data.dataset()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
 
 def running(epoch_n):
     for epoch in range(epoch_n):
@@ -9,3 +17,25 @@ def running(epoch_n):
 
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 통계를 출력합니다.
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+if __name__ == '__main__':
+    running(5)
+    PATH = './cifar_net.pth'
+    torch.save(net.state_dict(), PATH)
+
